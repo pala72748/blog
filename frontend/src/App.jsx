f3767e8d7fda51f8cab808e57ce4459f3f42aac6
pala72748 <pala72748@gmail.com>
2024-11-05 21:00:40 +0530

    toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,19 @@ import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <>
-      <ToastContainer />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition:Bounce
+      />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
